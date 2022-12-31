36c44a11a6a3c7321ae9b30334c48622a52758e5
garronej <joseph.garrone@data.gouv.fr>
2022-12-31 09:57:32 +0100

    @mui/x-grid is not the culpit

--- app/mui/page.tsx
+++ app/mui/page.tsx
@@ -42,8 +42,6 @@ import Stepper from '@mui/material/Stepper';
 import Step from '@mui/material/Step';
 import StepLabel from '@mui/material/StepLabel';
 
-import { DataGrid, GridColDef, GridValueGetterParams } from '@mui/x-data-grid';
-
 import Link from '@mui/material/Link';
 
 import { styled } from '@mui/material/styles';
@@ -75,7 +73,6 @@ export default function Mui() {
 			<Typography sx={{ mt: 2 }} variant="h4">
 				This is a place for testing MUI components
 			</Typography>
-			<DataGridDemo />
 			<BasicButtons />
 			<ComboBox />
 			<BasicChips />
@@ -550,76 +547,6 @@ const { HorizontalLinearStepper } = (() => {
 
 })();
 
-
-const { DataGridDemo } = (() => {
-
-
-
-	const columns: GridColDef[] = [
-		{ field: 'id', headerName: 'ID', width: 90 },
-		{
-			field: 'firstName',
-			headerName: 'First name',
-			width: 150,
-			editable: true,
-		},
-		{
-			field: 'lastName',
-			headerName: 'Last name',
-			width: 150,
-			editable: true,
-		},
-		{
-			field: 'age',
-			headerName: 'Age',
-			type: 'number',
-			width: 110,
-			editable: true,
-		},
-		{
-			field: 'fullName',
-			headerName: 'Full name',
-			description: 'This column has a value getter and is not sortable.',
-			sortable: false,
-			width: 160,
-			valueGetter: (params: GridValueGetterParams) =>
-				`${params.row.firstName || ''} ${params.row.lastName || ''}`,
-		},
-	];
-
-	const rows = [
-		{ id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
-		{ id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
-		{ id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
-		{ id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
-		{ id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
-		{ id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
-		{ id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
-		{ id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
-		{ id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
-	];
-
-	function DataGridDemo() {
-		return (
-			<Box sx={{ height: 400, width: '100%', mt: 7 }}>
-				<DataGrid
-					rows={rows}
-					columns={columns}
-					pageSize={5}
-					rowsPerPageOptions={[5]}
-					checkboxSelection
-					disableSelectionOnClick
-					experimentalFeatures={{ newEditingApi: true }}
-				/>
-			</Box>
-		);
-	}
-
-	return { DataGridDemo };
-
-
-
-})();
 
 const { Links } = (() => {
 
