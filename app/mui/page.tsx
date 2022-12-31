d0a4d8b24eee68a23fe456df4dac98d7d621484c
garronej <joseph.garrone@data.gouv.fr>
2022-12-31 10:28:47 +0100

    the switch only is engouth

--- app/mui/page.tsx
+++ app/mui/page.tsx
@@ -2,7 +2,6 @@
 
 import * as React from "react";
 import { useIsDark } from "@codegouvfr/react-dsfr/useIsDark";
-import Typography from "@mui/material/Typography";
 import FormControlLabel from '@mui/material/FormControlLabel';
 import Switch from '@mui/material/Switch';
 
@@ -20,9 +19,6 @@ export default function Mui() {
 				onChange={event => setIsDark(event.target.checked)}
 				inputProps={{ 'aria-label': 'controlled' }}
 			/>} label="Dark mode" />
-			<Typography sx={{ mt: 2 }} variant="h4">
-				This is a place for testing MUI components
-			</Typography>
 		</>
 	);
 }
