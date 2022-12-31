d4a2a41b2171d8b2beb1a96c380c42c1dfb69147
garronej <joseph.garrone@data.gouv.fr>
2022-12-31 11:43:22 +0100

    only a badge is sufficient

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,25 +1,20 @@
 import { NextAppDirEmotionCacheProvider } from "tss-react/next";
-import { DsfrHead } from "@codegouvfr/react-dsfr/next-appdir/DsfrHead";
-import { Footer } from "@codegouvfr/react-dsfr/Footer";
+import { Badge } from "@codegouvfr/react-dsfr/Badge";
+import "@codegouvfr/react-dsfr/dsfr/dsfr.css";
 
 export default function RootLayout({ children }: { children: React.ReactNode; }) {
 
 	return (
-		<html>
-			<head>
+		<html data-fr-theme="dark">
+			<head >
 				<title>Next 13 AppDir Demo DSFR setup</title>
-				<DsfrHead
-					defaultColorScheme="dark"
-				/>
 			</head>
 			<body>
 				<NextAppDirEmotionCacheProvider options={{ "key": "css" }}>
 					{children}
-					<Footer
-						brandTop={<>INTITULE<br />OFFICIEL</>}
-						accessibility="fully compliant"
-						contentDescription={""}
-						homeLinkProps={{ "href": "/", "title": "Accueil - Nom de l’entité (ministère, secrétariat d‘état, gouvernement)" }}
+					<Badge
+						label="Label badge"
+						severity="success"
 					/>
 				</NextAppDirEmotionCacheProvider>
 			</body>
