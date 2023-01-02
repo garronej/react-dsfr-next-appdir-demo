a85ffee28b4da4066edafc83b25dec085ede341c
garronej <joseph.garrone@data.gouv.fr>
2023-01-02 02:43:10 +0100

    add some logs

--- README.md
+++ README.md
@@ -1,15 +1,12 @@
 
-Run the App:  
+Reproduction repo 
 
-
-The App is live [here](https://react-dsfr-next-appdir-demo.vercel.app/)
 
 ```bash
 git clone https://github.com/garronej/react-dsfr-next-appdir-demo
 cd react-dsfr-next-appdir-demo
-yarn
-yarn build
-yarn start
+git checkout repaly
+rm -rf .next && yarn build && yarn start
 ```
 
 
--- app/page.tsx
+++ app/page.tsx
@@ -4,17 +4,36 @@ import React, { useState, useEffect } from "react";
 import FormControlLabel from '@mui/material/FormControlLabel';
 import Switch from '@mui/material/Switch';
 
+const isBrowser = typeof window === "object" && typeof document === "object";
+
 export default function Mui() {
 
 	const [isSomething, setIsSomething] = useState(false);
 
 	useEffect(
-		()=> {
+		() => {
 			setIsSomething(true);
 		},
 		[]
 	);
 
+	useState(
+		() => {
+
+
+			if (isBrowser) {
+
+				queueMicrotask(() => {
+					console.log("micro task after client side render");
+				});
+
+			}
+		}
+	);
+
+	if( isSomething ){
+		console.log("successful hydration");
+	}
 
 	return (
 		<>
@@ -22,7 +41,7 @@ export default function Mui() {
 				checked={isSomething}
 				onChange={event => setIsSomething(event.target.checked)}
 				inputProps={{ 'aria-label': 'controlled' }}
-			/>} label="Dark mode" />
+			/>} label="Something" />
 		</>
 	);
 }
