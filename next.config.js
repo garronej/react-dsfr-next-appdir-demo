83890667aa9b8df734abfb968b9ea1146e6f9bb3
garronej <joseph.garrone@data.gouv.fr>
2023-06-29 12:40:55 +0200

    Update

--- next.config.js
+++ next.config.js
@@ -1,12 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true, // Recommended for the `pages` directory, default in `app`.
-  swcMinify: false,
-  productionBrowserSourceMaps: true,
-  experimental: {
-    // Required:
-    appDir: true,
-  },
   webpack: config => {
 
     config.module.rules.push({
